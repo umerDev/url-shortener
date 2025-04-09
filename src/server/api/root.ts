7746ed91d03142c0919d38a23504ffe5673c82d6
umerDev <umerraja@live.co.uk>
2025-04-09 10:01:44 +0100

    feat: create url route

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { urlRouter } from "~/server/api/routers/url";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  url: urlRouter,
 });
 
 // export type definition of API
@@ -17,7 +17,7 @@ export type AppRouter = typeof appRouter;
  * Create a server-side caller for the tRPC API.
  * @example
  * const trpc = createCaller(createContext);
- * const res = await trpc.post.all();
+ * const res = await trpc.url.getLatest();
  *       ^? Post[]
  */
 export const createCaller = createCallerFactory(appRouter);
---
+++ src/server/api/routers/url.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const urlRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  create: publicProcedure
+    .input(z.object({ url: z.string().url() }))
+    .mutation(async ({ ctx, input }) => {
+      // Generate a unique hash for the URL
+      const url_hash = Math.random().toString(36).substring(2, 8);
+      
+      return ctx.db.uRL.create({
+        data: {
+          url: input.url,
+          url_hash: url_hash,
+        },
+      });
+    }),
+
+  getLatest: publicProcedure.query(async ({ ctx }) => {
+    const url = await ctx.db.uRL.findFirst({
+      orderBy: { createdAt: "desc" },
+    });
+
+    return url ?? null;
+  }),
+});
