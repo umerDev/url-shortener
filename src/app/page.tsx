2600aa628b4a0b2e103a621ecb76afafce2bb550
umerDev <umerraja@live.co.uk>
2025-04-09 10:52:46 +0100

    chore: delete unused call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,7 @@ import {GetAllUrls } from "~/app/_components/GetAllUrls";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.url.hello({ text: "from URL Shortener" });
-
   void api.url.getLatest.prefetch();
-
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
