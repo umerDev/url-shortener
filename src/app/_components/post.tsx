0f668efa2fef499ac52a5be66f247d55aa410a1f
umerDev <umerraja@live.co.uk>
2025-04-09 10:02:55 +0100

    feat: form submission for url

--- src/app/_components/post.tsx
+++ src/app/_components/post.tsx
@@ -4,45 +4,56 @@ import { useState } from "react";
 
 import { api } from "~/trpc/react";
 
-export function LatestPost() {
-  const [latestPost] = api.post.getLatest.useSuspenseQuery();
+export function UrlShortener() {
+  const [latestUrl] = api.url.getLatest.useSuspenseQuery();
 
   const utils = api.useUtils();
-  const [name, setName] = useState("");
-  const createPost = api.post.create.useMutation({
+  const [longUrl, setLongUrl] = useState("");
+  const createUrl = api.url.create.useMutation({
     onSuccess: async () => {
-      await utils.post.invalidate();
-      setName("");
+      await utils.url.invalidate();
+      setLongUrl("");
     },
   });
 
   return (
     <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
+      {latestUrl ? (
+        <p className="truncate mb-4">
+          Success! Here is your shortened URL: <br />
+          <a 
+            href={latestUrl.url} 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-blue-300 hover:underline"
+          >
+            {window.location.origin}/{latestUrl.url_hash}
+          </a>
+        </p>
       ) : (
-        <p>You have no posts yet.</p>
+        <p className="mb-4">You haven't shortened any URLs yet.</p>
       )}
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          createPost.mutate({ name });
+          createUrl.mutate({ url: longUrl });
         }}
         className="flex flex-col gap-2"
       >
         <input
-          type="text"
-          placeholder="Title"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          type="url"
+          placeholder="Enter a URL to shorten"
+          value={longUrl}
+          onChange={(e) => setLongUrl(e.target.value)}
           className="w-full rounded-full bg-white/10 px-4 py-2 text-white"
+          required
         />
         <button
           type="submit"
           className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-          disabled={createPost.isPending}
+          disabled={createUrl.isPending}
         >
-          {createPost.isPending ? "Submitting..." : "Submit"}
+          {createUrl.isPending ? "Shortening..." : "Shorten URL"}
         </button>
       </form>
     </div>
